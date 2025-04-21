1754c39bdfb014ff1f5e5ad6bb6dbb3899870f4c
earendil <zhu31415926535@126.com>
2025-04-21 17:29:14 +0800

    [feat](question):添加相关接口代码

---
+++ src/main/java/org/buaa/project/common/enums/QuestionErrorCodeEnum.java
@@ -0,0 +1,32 @@
+package org.buaa.project.common.enums;
+
+import org.buaa.project.common.convention.errorcode.IErrorCode;
+
+/**
+ * 系统错误码
+ */
+public enum QuestionErrorCodeEnum implements IErrorCode {
+
+    QUESTION_NULL("C000101", "该提问不存在"),
+    QUESTION_USER_INCORRECT("C000102","该提问并非该用户所有");
+
+    private final String code;
+
+    private final String message;
+
+    QuestionErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+
+}
--- src/main/java/org/buaa/project/service/impl/QuestionServiceImpl.java
+++ src/main/java/org/buaa/project/service/impl/QuestionServiceImpl.java
@@ -1,12 +1,15 @@
 package org.buaa.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.buaa.project.common.biz.user.UserContext;
+import org.buaa.project.common.convention.exception.ServiceException;
 import org.buaa.project.dao.entity.QuestionDO;
+import org.buaa.project.dao.entity.UserDO;
 import org.buaa.project.dao.mapper.QuestionMapper;
 import org.buaa.project.dto.req.question.QuestionSolveReqDTO;
 import org.buaa.project.dto.req.question.QuestionUpdateReqDTO;
@@ -14,6 +17,9 @@ import org.buaa.project.dto.req.question.QuestionUploadReqDTO;
 import org.buaa.project.service.QuestionService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import static org.buaa.project.common.enums.QuestionErrorCodeEnum.QUESTION_NULL;
+import static org.buaa.project.common.enums.QuestionErrorCodeEnum.QUESTION_USER_INCORRECT;
+import java.util.Objects;
 
 /**
  * The type Question service.
@@ -68,11 +74,19 @@ public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, QuestionDO>
 
     @Override
     public void checkQuestionExist(Long id) {
-
+        QuestionDO question = baseMapper.selectById(id);
+        if (Objects.isNull(question)) {
+            throw new ServiceException(QUESTION_NULL);
+        }
     }
 
     @Override
     public void checkQuestionOwner(Long id) {
-
+        checkQuestionExist(id);
+        Long currentUserId = UserContext.getUserId();
+        QuestionDO question = baseMapper.selectById(id);
+        if (!Objects.equals(currentUserId, question.getUserId())) {
+            throw new ServiceException(QUESTION_USER_INCORRECT);
+        }
     }
 }
