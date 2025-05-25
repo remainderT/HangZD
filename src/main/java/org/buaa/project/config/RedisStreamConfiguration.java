6a5d57b5620cc339894d7129d45a9733cffdb614
ysh <2692592600@qq.com>
2025-05-25 15:12:56 +0800

    # This is a combination of 2 commits. # This is the 1st commit message:
    fix: stream 问题
    
    This reverts commit fbb85e75a25c8e6bf85e7195025c01f72e2c1555.

--- src/main/java/org/buaa/project/config/RedisStreamConfiguration.java
+++ src/main/java/org/buaa/project/config/RedisStreamConfiguration.java
@@ -24,6 +24,7 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.buaa.project.common.consts.RedisCacheConstants.MESSAGE_SEND_STREAM_KEY;
 
 /**
@@ -40,39 +41,34 @@ public class RedisStreamConfiguration {
 
     @PostConstruct
     public void init() {
-        createStreamAndGroupIfNotExists(MESSAGE_SEND_STREAM_KEY, "message-send-consumer-group");
-    }
-
-    private void createStreamAndGroupIfNotExists(String streamKey, String groupName) {
-        StreamOperations<String, String, String> streamOperations = stringRedisTemplate.opsForStream();
-        if (!stringRedisTemplate.hasKey(streamKey)) {
-            Map<String, String> message = new HashMap<>();
-            message.put("status", "initialization");
-            streamOperations.add(streamKey, message);
+        StreamOperations<String, String, String> ops = stringRedisTemplate.opsForStream();
+        if (!stringRedisTemplate.hasKey(MESSAGE_SEND_STREAM_KEY)) {
+            Map<String, String> msg = new HashMap<>();
+            msg.put("status", "init");
+            ops.add(MESSAGE_SEND_STREAM_KEY, msg);
         }
-
         try {
-            streamOperations.createGroup(streamKey, ReadOffset.from("0-0"), groupName);
+            ops.createGroup(MESSAGE_SEND_STREAM_KEY, ReadOffset.from("0-0"), "message-send-consumer-group");
         } catch (Exception e) {
-            // 如果消费者组已经存在，忽略异常
             if (!e.getMessage().contains("BUSYGROUP")) {
                 throw e;
             }
         }
-        log.info("Stream {} and group {} initialization completed", streamKey, groupName);
+        log.info("Stream init done");
     }
+
 
     @Bean
     public ExecutorService asyncStreamConsumer() {
         AtomicInteger index = new AtomicInteger();
-        return new ThreadPoolExecutor(2,
-                2,
+        return new ThreadPoolExecutor(1,
+                1,
                 60,
                 TimeUnit.SECONDS,
                 new SynchronousQueue<>(),
                 runnable -> {
                     Thread thread = new Thread(runnable);
-                    thread.setName("stream_consumer_" + index.incrementAndGet());
+                    thread.setName("stream_consumer_stats_" + index.incrementAndGet());
                     thread.setDaemon(true);
                     return thread;
                 },
@@ -81,30 +77,23 @@ public class RedisStreamConfiguration {
     }
 
     @Bean
-    public Subscription shortLinkStatsSaveConsumerSubscription(ExecutorService asyncStreamConsumer) {
-        return createStreamConsumer(MESSAGE_SEND_STREAM_KEY, "message-send-consumer-group", asyncStreamConsumer, "consumer1");
-    }
-
-
-    private Subscription createStreamConsumer(String streamKey, String groupKey, ExecutorService asyncStreamConsumer, String consumerName) {
+    public Subscription ConsumerSubscription(ExecutorService asyncStreamConsumer) {
         StreamMessageListenerContainer.StreamMessageListenerContainerOptions<String, MapRecord<String, String, String>> options =
                 StreamMessageListenerContainer.StreamMessageListenerContainerOptions
                         .builder()
-                        // 批量拉取消息的大小为 10 条
+                        // 一次最多获取多少条消息
                         .batchSize(10)
+                        // 执行从 Stream 拉取到消息的任务流程
                         .executor(asyncStreamConsumer)
-                        // 设置拉取消息的超时时间为 3 秒
+                        // 如果没有拉取到消息，需要阻塞的时间。不能大于 ${spring.data.redis.timeout}，否则会超时
                         .pollTimeout(Duration.ofSeconds(3))
                         .build();
-
         StreamMessageListenerContainer.StreamReadRequest<String> streamReadRequest =
-                StreamMessageListenerContainer.StreamReadRequest.builder(StreamOffset.create(streamKey, ReadOffset.lastConsumed()))
+                StreamMessageListenerContainer.StreamReadRequest.builder(StreamOffset.create(MESSAGE_SEND_STREAM_KEY, ReadOffset.lastConsumed()))
                         .cancelOnError(throwable -> false)
-                        .consumer(Consumer.from(groupKey, consumerName))
-                        // 自动确认消息
+                        .consumer(Consumer.from("message-send-consumer-group", "stats-consumer"))
                         .autoAcknowledge(true)
                         .build();
-
         StreamMessageListenerContainer<String, MapRecord<String, String, String>> listenerContainer = StreamMessageListenerContainer.create(redisConnectionFactory, options);
         Subscription subscription = listenerContainer.register(streamReadRequest, mqConsumer);
         listenerContainer.start();
