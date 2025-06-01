9d3d480393b60f4d0d4d648618537fa28e2e0e80
earendil <zhu31415926535@126.com>
2025-06-01 13:48:00 +0800

    [feat](websocket)成功部署websocket功能

---
+++ src/main/java/org/buaa/project/common/websocket/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package org.buaa.project.common.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final MyWebSocketHandler myWebSocketHandler;
+
+    public WebSocketConfig(MyWebSocketHandler myWebSocketHandler) {
+        this.myWebSocketHandler = myWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler, "/ws")
+                .setAllowedOrigins("*"); // 允许跨域
+    }
+}
